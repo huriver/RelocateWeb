cc8d44de0654af72b5aa79cf2b1fb848c9676eb0
赵鹏程 <zhao.pengcheng31@iwhalecloud.com>
2025-04-14 21:17:51 +0800

    初始化后端框架

---
+++ background/src/main/java/com/ahut/controller/admin/testController.java
@@ -0,0 +1,19 @@
+package com.***REMOVED***.controller.admin;
+
+import com.***REMOVED***.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class testController {
+
+    @GetMapping("/test")
+    public Result test(){
+        log.info("test");
+        return Result.success("test");
+    }
+}
---
+++ background/src/test/java/com/ahut/BackgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.***REMOVED***;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
