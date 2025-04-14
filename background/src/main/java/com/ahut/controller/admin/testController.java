de07094575270f15726b2370600b6be0a64d933d
赵鹏程 <zhao.pengcheng31@iwhalecloud.com>
2025-04-14 21:17:51 +0800

    初始化后端框架

---
+++ background/src/main/java/com/ahut/controller/admin/testController.java
@@ -0,0 +1,19 @@
+package com.ahut.controller.admin;
+
+import com.ahut.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class testController {
+
+    @GetMapping("/test")
+    public Result test(){
+        log.info("test");
+        return Result.success("test");
+    }
+}
