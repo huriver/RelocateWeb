f3d65e5e83de8d5c59289fb40f6e9f73dcd609ca
赵鹏程 <zhao.pengcheng31@iwhalecloud.com>
2025-04-15 16:00:47 +0800

    实现搬家工人登录功能

---
+++ background/src/main/java/com/ahut/service/MoverService.java
@@ -0,0 +1,8 @@
+package com.***REMOVED***.service;
+
+import com.***REMOVED***.dto.UserLoginDTO;
+import com.***REMOVED***.entity.Mover;
+
+public interface MoverService {
+    Mover login(UserLoginDTO userLoginDTO);
+}
---
+++ background/src/main/java/com/ahut/service/impl/MoverServiceImpl.java
@@ -0,0 +1,47 @@
+package com.***REMOVED***.service.impl;
+
+import com.***REMOVED***.constant.MessageConstant;
+import com.***REMOVED***.dto.UserLoginDTO;
+import com.***REMOVED***.entity.Mover;
+import com.***REMOVED***.exception.AccountNotFoundException;
+import com.***REMOVED***.exception.PasswordErrorException;
+import com.***REMOVED***.mapper.MoverMapper;
+import com.***REMOVED***.service.MoverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+@Service
+public class MoverServiceImpl implements MoverService {
+
+    @Autowired
+    private MoverMapper moverMapper;
+
+
+    @Override
+    public Mover login(UserLoginDTO userLoginDTO) {
+        String username = userLoginDTO.getUsername();
+        String password = userLoginDTO.getPassword();
+
+        //1、根据用户名查询数据库中的数据
+        Mover mover= moverMapper.getByUsername(username);
+
+        //2、处理各种异常情况（用户名不存在、密码不对）
+        if (mover == null) {
+            //账号不存在
+            throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
+        }
+
+        //密码比对
+        //后期需要进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!password.equals(mover.getPassword())) {
+            //密码错误
+            throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
+        }
+
+        //3、返回实体对象
+        return mover;
+    }
+
+}
